e08b0fd5dd56f1587ce37bdefe4c0914f129925f
MRedor <mashapalchenkova@gmail.com>
2020-11-08 20:09:35 +0300

    add fake head

--- Queue.h
+++ Queue.h
@@ -51,76 +51,42 @@ public:
         this->node = (int*) numa_alloc_onnode(sizeof(int), node);
         *this->node = node;
 
+        auto fake = (Element*) numa_alloc_onnode(sizeof(Element), node);
+
         head = (std::atomic<Element*> *) numa_alloc_onnode(sizeof(std::atomic<Element*>), node);
-        head->store(nullptr);
+        head->store(fake);
         tail = (std::atomic<Element*> *) numa_alloc_onnode(sizeof(std::atomic<Element*>), node);
-        tail->store(nullptr);
+        tail->store(fake);
     }
 
     void Push(std::pair<int, int> p) {
-        m.lock();
         Element* e = (Element*) numa_alloc_onnode(sizeof(Element), *node);
         e->Init(p, *node);
-
-        if (tail->load() == nullptr) {
-            head->store(e);
-            tail->store(e);
-        } else {
-            tail->load()->SetNext(e);
-            tail->store(e);
-        }
+        m.lock();
+        tail->load()->SetNext(e);
+        tail->store(e);
         m.unlock();
     }
-
-//    std::vector<std::pair<int, int>> List() {
-//        m.lock();
-//        //std::cerr << "in Queue List \n";
-//        std::vector<std::pair<int, int>> result;
-//        while (!empty()) {
-//            //std::cerr << "in while \n";
-//            auto p = pop();
-//            if (!p) {
-//                break;
-//            }
-//            //std::cerr << p->first << " " << p->second << " poped \n";
-//
-//            result.emplace_back(*p);
-//        }
-//        m.unlock();
-//        return result;
-//    }
 
     std::pair<int, int>* Pop() {
         //std::cerr << "in pop \n";
         m.lock();
-        if (head->load() == nullptr) {
+        if (head->load() == tail->load()) {
             m.unlock();
             return nullptr;
         }
 
         auto e = (std::pair<int, int> *) numa_alloc_onnode(sizeof(std::pair<int, int>), *node);
-
-        //auto e = new std::pair<int, int>(*head->load()->GetFirst(), *head->load()->GetSecond());
-        e->first = *head->load()->GetFirst();
-        e->second = *head->load()->GetSecond();
+        e->first = *head->load()->GetNext()->GetFirst();
+        e->second = *head->load()->GetNext()->GetSecond();
 
         head->store(head->load()->GetNext());
-        if (head->load() == nullptr) {
-            tail->store(nullptr);
-        }
         m.unlock();
 
         return e;
     }
 
 private:
-    bool empty() {
-        if (head->load() == nullptr) {
-            return true;
-        }
-        return false;
-    }
-
     int* node;
     std::atomic<Element*>* head;
     std::atomic<Element*>* tail;
