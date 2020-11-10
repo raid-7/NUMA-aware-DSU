dfd603ddd138a2602b1e28df470174a0b3fa12fe
MRedor <mashapalchenkova@gmail.com>
2020-11-10 20:10:28 +0300

    separate mutex for nodes

--- DSU.h
+++ DSU.h
@@ -16,14 +16,14 @@ public:
         queues.resize(node_count);
         for (int i = 0; i < node_count; i++) {
             data[i] = (std::atomic<int> *) numa_alloc_onnode(sizeof(std::atomic<int>) * size, i);
-            //data[i] = (int*) numa_alloc_onnode(sizeof(int) * size, i);
             for (int j = 0; j < size; j++) {
-                //data[i][j] = j;
                 data[i][j].store(j);
             }
 
             queues[i] = (Queue*) numa_alloc_onnode(sizeof(Queue), i);
             queues[i]->Init(i);
+
+            mutexes[i] = (std::mutex*) numa_alloc_onnode(sizeof(std::mutex), i);
         }
     }
 
@@ -31,39 +31,37 @@ public:
         for (int i = 0; i < node_count; i++) {
             numa_free(data[i], sizeof(int) * size);
             numa_free(queues[i], sizeof(Queue));
+            numa_free(mutexes[i], sizeof(std::mutex));
         }
     }
 
     void Union(int u, int v) {
         //std::string output = std::to_string(sched_getcpu()) + " in union \n";
         //std::cerr << output;
-        m.lock();
+        auto cpu = sched_getcpu();
+        auto node = numa_node_of_cpu(cpu);
+        mutexes[node]->lock();
         if (node_count > 1) {
-            auto cpu = sched_getcpu();
-            auto node = numa_node_of_cpu(cpu);
-            //std::cerr << sched_getcpu() << " " << "got cpu and node \n";
             for (int i = 0; i < node_count; i++) {
                 if (i == node)
                     continue;
-                //std::cerr << sched_getcpu() << " " << "want to push in queue \n";
                 queues[i]->Push(std::make_pair(u, v));
             }
-
             union_(u, v, node);
         } else {
             union_(u, v, 0);
         }
 
-        m.unlock();
+        mutexes[node]->unlock();
     }
 
     bool SameSet(int u, int v) {
         if (node_count > 1) {
             auto cpu = sched_getcpu();
             auto node = numa_node_of_cpu(cpu);
-            m.lock();
+            mutexes[node]->lock();
             old_unions(node);
-            m.unlock();
+            mutexes[node]->unlock();
             return find(u, node) == find(v, node);
         } else {
             return find(u, 0) == find(v, 0);
@@ -74,9 +72,9 @@ public:
         if (node_count > 1) {
             auto cpu = sched_getcpu();
             auto node = numa_node_of_cpu(cpu);
-            m.lock();
+            mutexes[node]->lock();
             old_unions(node);
-            m.unlock();
+            mutexes[node]->unlock();
             return find(u, node);
         } else {
             return find(u, 0);
@@ -84,10 +82,10 @@ public:
     }
 
     bool __SameSetOnNode(int u, int v, int node) {
-        m.lock();
+        mutexes[node]->lock();
         old_unions(node);
         auto res = (find(u, node) == find(v, node));
-        m.unlock();
+        mutexes[node]->unlock();
         return res;
     }
 
@@ -103,12 +101,10 @@ private:
     }
 
     int find(int u, int node) {
-        //auto par = data[node][u];//.load();
         auto par = data[node][u].load();
         while (par != data[node][par].load()) {
             par = data[node][par].load();
         }
-        //std::cerr << sched_getcpu() << " " << "parent found \n";
         return par;
     }
 
@@ -120,10 +116,8 @@ private:
         }
         if (rand() % 2) {
             data[node][u_p].store(v_p);
-            //while (!data[node][u].compare_exchange_weak(u, v)) {}
         } else {
-            //while (!data[node][v].compare_exchange_weak(v, u)) {}
-            data[node][v_p].store(u_p);// = u_p;
+            data[node][v_p].store(u_p);
         }
     }
 
@@ -131,10 +125,9 @@ private:
     int size;
     int node_count;
     std::vector<std::atomic<int>*> data;
-    //std::vector<int*> data;
     std::vector<Queue*> queues;
 
-    std::mutex m;
+    std::vector<std::mutex*> mutexes;
 };
 
 
