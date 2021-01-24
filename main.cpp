6a55457516d5c3db223d52be6cc562a49e09d2ba
Mariia <mariia.chukhareva@gmail.com>
2021-01-24 18:52:17 +0300

    add multiple runs

--- main.cpp
+++ main.cpp
@@ -9,14 +9,10 @@
 const int N = 1e9;
 const int THREADS = 10;
 
-void thread_routine(std::atomic_int* counter, int* time) {
-//    auto start = std::chrono::high_resolution_clock::now();
+void thread_routine(std::atomic_int* counter) {
     for (int i = 0; i < N / THREADS; i++) {
         counter->fetch_add(1);
     }
-//    auto stop = std::chrono::high_resolution_clock::now();
-//    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-//    *time = duration.count();
 }
 
 //here we believe that configured nodes numbered with 0 to n (number of them)
@@ -36,18 +32,24 @@ void test(int node_count) {
             numa_bitmask_setbit(bindMaskToRun, run_node);
             numa_bind(bindMaskToRun);
 
-            std::vector<std::thread> threads;
-            auto start = std::chrono::high_resolution_clock::now();
-            for (int i = 0; i < THREADS; i++) {
-                threads.emplace_back(std::thread(thread_routine, counter, &times[i]));
-            }
-            for (int i = 0; i < THREADS; i++) {
-                threads[i].join();
-            }
-            auto stop = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            int runs = 3;
+            int sum_time = 0;
+            for (int r = 0; r < runs; r++) {
+                std::vector<std::thread> threads;
+                auto start = std::chrono::high_resolution_clock::now();
+                for (int i = 0; i < THREADS; i++) {
+                    threads.emplace_back(std::thread(thread_routine, counter));
+                }
+                for (int i = 0; i < THREADS; i++) {
+                    threads[i].join();
+                }
+                auto stop = std::chrono::high_resolution_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+                sum_time += duration.count();
 
-            std::cout << "Time for run on node " << run_node << ": " << duration.count() << " milliseconds" << std::endl;
+                counter->store(0);
+            }
+            std::cout << "Time for run on node " << run_node << ": " << float(sum_time) / runs << " milliseconds" << std::endl;
 
             numa_free(counter, sizeof(std::atomic_int));
             numa_free(times, sizeof(int) * THREADS);
@@ -62,8 +64,6 @@ int main() {
         std::cout << "NO NUMA HERE" << std::endl;
         return 0;
     } else {
-        test(numa_num_configured_nodes());
-        std::cout << "\n-------------------\n\n";
         test(numa_num_configured_nodes());
     }
 }
