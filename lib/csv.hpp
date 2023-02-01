058e46c23209309d33a860c8947034f41bc2f6cd
Raed Romanov <raid_r@mail.ru>
2023-02-01 03:45:43 +0300

    Fix record writer move

--- lib/csv.hpp
+++ lib/csv.hpp
@@ -10,15 +10,27 @@ public:
     class RecordWriter {
     public:
         RecordWriter(std::ostream& stream)
-            : stream_(stream) {}
+                : stream_(stream) {}
+
+        RecordWriter(RecordWriter&& oth)
+                : stream_(oth.stream_)
+                , somethingEmitted_(oth.somethingEmitted_) {
+            oth.moved_ = true;
+        }
 
         template<class T>
-        RecordWriter& operator <<(const T& value) {
+        RecordWriter& operator <<(const T& value) & {
             if (somethingEmitted_)
                 stream_ << ',';
             stream_ << value;
             somethingEmitted_ = true;
             return *this;
+        }
+
+        template<class T>
+        RecordWriter&& operator <<(const T& value) && {
+            *this << value;
+            return std::move(*this);
         }
 
         template<class R, class... Args>
@@ -28,13 +40,16 @@ public:
         }
 
         ~RecordWriter() {
-            stream_ << "\n";
-            stream_.flush();
+            if (!moved_) {
+                stream_ << "\n";
+                stream_.flush();
+            }
         }
 
     private:
         std::ostream& stream_;
         bool somethingEmitted_ = false;
+        bool moved_ = false;
     };
 
     CsvFile(std::string path)
